6c66e7e9dc5a9fa917333e8e2851d7e13b58af7d
fannairu <fannairu@gmail.com>
2017-01-18 18:28:24 +0800

    refine

--- service/document.go
+++ service/document.go
@@ -81,9 +81,9 @@ func DocumentStatistics(start time.Time, end time.Time) []*model.Statistic {
 	group := bson.M{}
 	match := bson.M{}
 	//date := bson.M{"$dateToString": bson.M{"format": "%Y-%m-%d", "date": "$createdTime"}}
-	year := bson.M{"$substr ": []interface{}{"$createdTime", 0, 4}}
-	month := bson.M{"$substr ": []interface{}{"$createdTime", 5, 2}}
-	day := bson.M{"$substr ": []interface{}{"$createdTime", 8, 2}}
+	year := bson.M{"$substr": []interface{}{"$createdTime", 0, 4}}
+	month := bson.M{"$substr": []interface{}{"$createdTime", 5, 2}}
+	day := bson.M{"$substr": []interface{}{"$createdTime", 8, 2}}
 	date := bson.M{"$concat": []interface{}{year, "-", month, "-", day}}
 	group["$group"] = bson.M{"_id": date, "count": bson.M{"$sum": 1}}
 	match["$match"] = bson.M{"createdTime": bson.M{"$gte": start, "$lte": end}}
