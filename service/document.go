6cc19148779d5f5554650379c60f8b131c53fca9
fannairu <fannairu@gmail.com>
2017-01-18 18:25:56 +0800

    refine

--- service/document.go
+++ service/document.go
@@ -81,7 +81,10 @@ func DocumentStatistics(start time.Time, end time.Time) []*model.Statistic {
 	group := bson.M{}
 	match := bson.M{}
 	//date := bson.M{"$dateToString": bson.M{"format": "%Y-%m-%d", "date": "$createdTime"}}
-	date := bson.M{"$concat": []interface{}{bson.M{"$year": "$createdTime"}, "-", bson.M{"month": "$createdTime"}, "-", bson.M{"dayOfMonth": "$createdTime"}}}
+	year := bson.M{"$substr ": []interface{}{"$createdTime", 0, 4}}
+	month := bson.M{"$substr ": []interface{}{"$createdTime", 5, 2}}
+	day := bson.M{"$substr ": []interface{}{"$createdTime", 8, 2}}
+	date := bson.M{"$concat": []interface{}{year, "-", month, "-", day}}
 	group["$group"] = bson.M{"_id": date, "count": bson.M{"$sum": 1}}
 	match["$match"] = bson.M{"createdTime": bson.M{"$gte": start, "$lte": end}}
 	session.MustPipeAll(collectionDocuments, []bson.M{match, group}, &results)
